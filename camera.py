42f8752d142e8026a31dcebd669125ec8d68fd9f
henrique <henriquesevdacunha@gmail.com>
2024-05-24 10:23:34 -0400

    arrumando o configuração de salvamento das imagens

--- camera.py
+++ camera.py
@@ -1,13 +1,17 @@
 from picamera2 import Picamera2
 from time import sleep
+from datetime import datetime
 
 picam2 = Picamera2()
 
-def tirar_foto(file_path='/home/fab/image.jpg'):
+def tirar_foto():
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = f'/home/pi/image_{timestamp}.jpg'
     picam2.start()
     sleep(2)  # Tempo para a câmera ajustar a exposição
     picam2.capture_file(file_path)
     picam2.stop()
+    print(f"Foto salva como {file_path}")
 
 def fechar_camera():
     picam2.close()
