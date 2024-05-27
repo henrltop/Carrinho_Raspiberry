8dcf9f2dcfe432cecda0bebc6c0d551d703f9e41
henrique <henriquesevdacunha@gmail.com>
2024-05-27 12:32:08 -0400

    eu literalmente to copiando da documentação, quero ver esse joça n funcionar agr

--- camera.py
+++ camera.py
@@ -1,33 +1,43 @@
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
-import os
+from time import sleep
 from datetime import datetime
+import os
+
+picam2 = Picamera2()
 
 def criar_diretorio_se_nao_existir(diretorio):
     if not os.path.exists(diretorio):
         os.makedirs(diretorio)
 
-picam2 = Picamera2()
-
 def tirar_foto():
-    criar_diretorio_se_nao_existir('/home/fab/Imagens')
-    nome_arquivo = '/home/fab/Imagens/foto_{}.jpg'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+    diretorio = '/home/fab/Imagens'
+    criar_diretorio_se_nao_existir(diretorio)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = os.path.join(diretorio, f'image_{timestamp}.jpg')
     picam2.start_preview(Preview.NULL)
-    picam2.capture_file(nome_arquivo)
-    print(f"Foto salva em: {nome_arquivo}")
+    picam2.capture_file(file_path)
+    picam2.stop_preview()
+    print(f"Foto salva como {file_path}")
 
 def iniciar_gravacao():
-    criar_diretorio_se_nao_existir('/home/fab/Vídeos')
-    nome_arquivo = '/home/fab/Vídeos/video_{}.h264'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
-    output = FileOutput(nome_arquivo)
-    picam2.start_recording(output)
-    print(f"Iniciando gravação... Arquivo: {nome_arquivo}")
-    return nome_arquivo
+    diretorio = '/home/fab/Vídeos'
+    criar_diretorio_se_nao_existir(diretorio)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_path = os.path.join(diretorio, f'video_{timestamp}.h264')
+    encoder = H264Encoder()
+    output = FileOutput(file_path)
+    picam2.start()
+    picam2.start_recording(encoder, output)
+    print(f"Gravação iniciada: {file_path}")
+    return file_path
 
 def parar_gravacao():
     picam2.stop_recording()
-    print("Gravação parada.")
+    picam2.stop()
+    print("Gravação parada")
 
 def fechar_camera():
     picam2.close()
-    print("Câmera fechada.")
+    print("Câmera fechada")
