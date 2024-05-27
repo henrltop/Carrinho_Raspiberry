6702aed36f6fee2e44ca93160365404ca34cb8cb
henrique <henriquesevdacunha@gmail.com>
2024-05-27 12:22:15 -0400

    corrijindo a camera segunda a documentação da biblioteca

--- camera.py
+++ camera.py
@@ -1,39 +1,33 @@
-from picamera2 import Picamera2
-from time import sleep
-from datetime import datetime
+from picamera2 import Picamera2, Preview
+from picamera2.outputs import FileOutput
 import os
-
-picam2 = Picamera2()
+from datetime import datetime
 
 def criar_diretorio_se_nao_existir(diretorio):
     if not os.path.exists(diretorio):
         os.makedirs(diretorio)
 
+picam2 = Picamera2()
+
 def tirar_foto():
-    diretorio = '/home/fab/Imagens'
-    criar_diretorio_se_nao_existir(diretorio)
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = os.path.join(diretorio, f'image_{timestamp}.jpg')
-    picam2.start()
-    sleep(2)  # Tempo para a câmera ajustar a exposição
-    picam2.capture_file(file_path)
-    picam2.stop()
-    print(f"Foto salva como {file_path}")
+    criar_diretorio_se_nao_existir('/home/fab/Imagens')
+    nome_arquivo = '/home/fab/Imagens/foto_{}.jpg'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+    picam2.start_preview(Preview.NULL)
+    picam2.capture_file(nome_arquivo)
+    print(f"Foto salva em: {nome_arquivo}")
 
 def iniciar_gravacao():
-    diretorio = '/home/fab/Vídeos'
-    criar_diretorio_se_nao_existir(diretorio)
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = os.path.join(diretorio, f'video_{timestamp}.h264')
-    picam2.start()
-    picam2.start_recording(file_path)
-    print(f"Gravação iniciada: {file_path}")
-    return file_path
+    criar_diretorio_se_nao_existir('/home/fab/Vídeos')
+    nome_arquivo = '/home/fab/Vídeos/video_{}.h264'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+    output = FileOutput(nome_arquivo)
+    picam2.start_recording(output)
+    print(f"Iniciando gravação... Arquivo: {nome_arquivo}")
+    return nome_arquivo
 
 def parar_gravacao():
     picam2.stop_recording()
-    picam2.stop()
-    print("Gravação parada")
+    print("Gravação parada.")
 
 def fechar_camera():
     picam2.close()
+    print("Câmera fechada.")
--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from camera import tirar_foto, iniciar_gravacao, parar_gravacao, fechar_camera
 from truques import volta_360
 
 # Inicializar o pygame
-print("Inicializando pygame...")
 pygame.init()
 
 # Inicializar o joystick
-print("Inicializando joystick...")
 pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
@@ -22,55 +20,49 @@ try:
             if event.type == pygame.JOYAXISMOTION:
                 # Eixo 0 (esquerda/direita) do joystick esquerdo para rotação
                 axis_0 = joystick.get_axis(0)
-                # Eixo 1 (cima/baixo) do joystick esquerdo para direção
-                axis_1 = joystick.get_axis(1)
                 # Eixo 3 (cima/baixo) do joystick direito para aceleração
                 axis_3 = joystick.get_axis(3)
                 
                 # Controle da velocidade com o joystick direito
                 velocidade = int((abs(axis_3) * 100))
-                print(f"Velocidade: {velocidade}")
 
                 # Controle do movimento
                 if axis_3 < -0.1:
-                    print("Movendo para frente...")
+                    print("Movendo para frente")
                     motores_frente(velocidade)
                 elif axis_3 > 0.1:
-                    print("Movendo para trás...")
+                    print("Movendo para trás")
                     motores_tras(velocidade)
                 else:
-                    print("Parando motores...")
+                    print("Parando")
                     motores_parar()
                 
                 # Controle da direção com o joystick esquerdo
                 if axis_0 < -0.1:
-                    print("Virando para a esquerda...")
+                    print("Virando à esquerda")
                     virar_esquerda(velocidade)
                 elif axis_0 > 0.1:
-                    print("Virando para a direita...")
+                    print("Virando à direita")
                     virar_direita(velocidade)
-                else:
-                    motores_parar()
 
             elif event.type == pygame.JOYBUTTONDOWN:
                 if event.button == 2:  # Botão X para tirar foto
-                    print("Tirando foto...")
+                    print("Tirando foto")
                     tirar_foto()
                 if event.button == 6:  # Botão RB para fazer uma volta de 360 graus
-                    print("Executando volta de 360 graus...")
+                    print("Realizando volta de 360 graus")
                     volta_360()
                 if event.button == 0:  # Botão A para iniciar/parar gravação
                     if not gravando:
-                        print("Iniciando gravação...")
+                        print("Iniciando gravação")
                         arquivo_video = iniciar_gravacao()
                         gravando = True
                     else:
-                        print("Parando gravação...")
+                        print("Parando gravação")
                         parar_gravacao()
                         gravando = False
 
 except KeyboardInterrupt:
-    print("Interrupção detectada. Limpando GPIO e fechando câmera...")
     cleanup()
     fechar_camera()
     pygame.quit()
