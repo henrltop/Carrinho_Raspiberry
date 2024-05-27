499c7a366f0e82785ad7f75fb81a32b05fafd225
henrique <henriquesevdacunha@gmail.com>
2024-05-27 10:39:20 -0400

    atualizando a camera de vez (EU ACHO)

--- camera.py
+++ camera.py
@@ -1,12 +1,19 @@
 from picamera2 import Picamera2
 from time import sleep
 from datetime import datetime
+import os
 
 picam2 = Picamera2()
 
+def criar_diretorio_se_nao_existir(diretorio):
+    if not os.path.exists(diretorio):
+        os.makedirs(diretorio)
+
 def tirar_foto():
+    diretorio = '/home/pi/fotos'
+    criar_diretorio_se_nao_existir(diretorio)
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = f'/home/pi/image_{timestamp}.jpg'
+    file_path = os.path.join(diretorio, f'image_{timestamp}.jpg')
     picam2.start()
     sleep(2)  # Tempo para a câmera ajustar a exposição
     picam2.capture_file(file_path)
@@ -14,10 +21,12 @@ def tirar_foto():
     print(f"Foto salva como {file_path}")
 
 def iniciar_gravacao():
+    diretorio = '/home/pi/videos'
+    criar_diretorio_se_nao_existir(diretorio)
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = f'/home/pi/video_{timestamp}.h264'
+    file_path = os.path.join(diretorio, f'video_{timestamp}.h264')
     picam2.start()
-    picam2.start_recording(file_path, output=file_path)
+    picam2.start_recording(file_path)
     print(f"Gravação iniciada: {file_path}")
     return file_path
 
