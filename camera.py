f63641cd96d1382f1db35bf0f990c546dad705b7
henrique <henriquesevdacunha@gmail.com>
2024-05-27 12:59:24 -0400

    atualizando a camera dnv

--- camera.py
+++ camera.py
@@ -1,4 +1,4 @@
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FileOutput
 from time import sleep
