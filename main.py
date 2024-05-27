e06b9e8ebb3c9061d32a6d3c8fa437ef248306ce
henrique <henriquesevdacunha@gmail.com>
2024-05-27 16:40:29 -0400

    atualizando de vez esta é a versão final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from rodas import motores_frente, motores_tras, motores_parar, virar_esquerda, virar_direita, cleanup
+from camera import tirar_foto, iniciar_gravacao, parar_gravacao, fechar_camera
 from truques import volta_360
 
 # Inicializar o pygame
@@ -10,8 +11,8 @@ pygame.joystick.init()
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
-# Variável para controle de gravação
 gravando = False
+arquivo_video = None
 
 try:
     while True:
@@ -21,14 +22,17 @@ try:
                 axis_0 = joystick.get_axis(0)
                 # Eixo 3 (cima/baixo) do joystick direito para aceleração
                 axis_3 = joystick.get_axis(3)
+                
+                # Controle da velocidade com o joystick direito
+                velocidade = int((abs(axis_3) * 100))
 
                 # Controle do movimento
                 if axis_3 < -0.1:
                     print("Movendo para frente")
-                    motores_frente()
+                    motores_frente(velocidade)
                 elif axis_3 > 0.1:
                     print("Movendo para trás")
-                    motores_tras()
+                    motores_tras(velocidade)
                 else:
                     print("Parando")
                     motores_parar()
@@ -36,18 +40,19 @@ try:
                 # Controle da direção com o joystick esquerdo
                 if axis_0 < -0.1:
                     print("Virando à esquerda")
-                    virar_esquerda()
+                    virar_esquerda(velocidade)
                 elif axis_0 > 0.1:
                     print("Virando à direita")
-                    virar_direita()
-                else:
-                    motores_parar()
+                    virar_direita(velocidade)
 
             elif event.type == pygame.JOYBUTTONDOWN:
+                if event.button == 2:  # Botão X para tirar foto
+                    print("Tirando foto")
+                    tirar_foto()
                 if event.button == 6:  # Botão RB para fazer uma volta de 360 graus
                     print("Realizando volta de 360 graus")
                     volta_360()
-                elif event.button == 0:  # Botão A para iniciar/parar gravação
+                if event.button == 0:  # Botão A para iniciar/parar gravação
                     if not gravando:
                         print("Iniciando gravação")
                         arquivo_video = iniciar_gravacao()
@@ -56,10 +61,8 @@ try:
                         print("Parando gravação")
                         parar_gravacao()
                         gravando = False
-                elif event.button == 2:  # Botão X para tirar foto
-                    print("Tirando foto")
-                    tirar_foto()
 
 except KeyboardInterrupt:
     cleanup()
+    fechar_camera()
     pygame.quit()
--- truques.py
+++ truques.py
@@ -2,6 +2,8 @@ from rodas import virar_direita, motores_parar
 from time import sleep
 
 def volta_360():
-    virar_direita(100)  # Velocidade máxima
-    sleep(2)  # Ajuste o tempo conforme necessário para uma rotação completa
+    velocidade = 100  # Velocidade máxima
+    print("Realizando volta de 360 graus")
+    virar_direita(velocidade)
+    sleep(2)  # Ajuste o tempo conforme necessário para completar a volta
     motores_parar()
