abfc038fca3608934a110a7d55aa844e79260db6
henrique <henriquesevdacunha@gmail.com>
2024-06-12 08:34:37 -0400

    atualizando teste

--- testes/camera.py
+++ testes/camera.py
@@ -1,46 +1,18 @@
-from picamera2 import Picamera2, Preview
-from picamera2.encoders import H264Encoder
-from picamera2.outputs import FileOutput
-from time import sleep
-from datetime import datetime
-import os
+import cv2
 
-picam2 = Picamera2()
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Erro ao abrir a câmera")
 
-def criar_diretorio_se_nao_existir(diretorio):
-    if not os.path.exists(diretorio):
-        os.makedirs(diretorio)
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("Erro ao ler o frame")
+        break
 
-def tirar_foto():
-    diretorio = '/home/fab/Imagens'
-    criar_diretorio_se_nao_existir(diretorio)
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = os.path.join(diretorio, f'image_{timestamp}.jpg')
-    picam2.start_preview(Preview.NULL)
-    picam2.start()
-    sleep(2)  # Tempo para a câmera ajustar a exposição
-    picam2.capture_file(file_path)
-    picam2.stop_preview()
-    picam2.stop()
-    print(f"Foto salva como {file_path}")
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-def iniciar_gravacao():
-    diretorio = '/home/fab/Vídeos'
-    criar_diretorio_se_nao_existir(diretorio)
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = os.path.join(diretorio, f'video_{timestamp}.h264')
-    encoder = H264Encoder()
-    output = FileOutput(file_path)
-    picam2.start()
-    picam2.start_recording(encoder, output)
-    print(f"Gravação iniciada: {file_path}")
-    return file_path
-
-def parar_gravacao():
-    picam2.stop_recording()
-    picam2.stop()
-    print("Gravação parada")
-
-def fechar_camera():
-    picam2.close()
-    print("Câmera fechada")
+cap.release()
+cv2.destroyAllWindows()
