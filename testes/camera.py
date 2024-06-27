bd3b04bcca6594d559b4c79cf58fff2427fb5249
henrique <henriquesevdacunha@gmail.com>
2024-06-27 11:42:25 -0400

    teste diferente

--- testes/camera.py
+++ testes/camera.py
@@ -1,20 +1,26 @@
 import cv2
 
-def show_camera():
-    camera = cv2.VideoCapture(0)
-    if not camera.isOpened():
+def open_camera():
+    cap = cv2.VideoCapture(0)
+    
+    if not cap.isOpened():
         print("Cannot open camera")
-        exit()
+        return
+    
     while True:
-        ret, frame = camera.read()
+        ret, frame = cap.read()
+        
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        cv2.imshow('Camera', frame)
+        
+        cv2.imshow('frame', frame)
+        
         if cv2.waitKey(1) == ord('q'):
             break
-    camera.release()
+    
+    cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    show_camera()
+    open_camera()
